f92e7e799a8e162263024a117f19ab7e86594289
basslines <admir.osmanovic@gmail.com>
2021-03-30 19:19:36 +0200

    dodao ex01 projekat

---
+++ ex01/src/Plant.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Plant extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { type: "Flowering plant" };
+  }
+  render() {
+    return (
+      <div>
+        <h2>This is a Plant Component!</h2>
+      </div>
+    );
+  }
+}
+
+export default Plant;
