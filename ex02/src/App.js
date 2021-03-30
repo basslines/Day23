9161f863653c4ba920f8a5016420659c65069342
basslines <admir.osmanovic@gmail.com>
2021-03-30 21:25:21 +0200

    dodao ex02 projekat

---
+++ ex02/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Plant from "./Plant";
+import "./Plant";
+
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
