421e85e71f0ddf84ef91e6e838dd369b98f034b3
basslines <admir.osmanovic@gmail.com>
2021-03-30 22:52:47 +0200

    dodao ex05 projekat

---
+++ ex05/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SimpleForm from "./SimpleForm";
+import "./SimpleForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleForm />
+    </div>
+  );
+}
+
+export default App;
